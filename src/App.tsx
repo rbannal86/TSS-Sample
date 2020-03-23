268a089a5d29a3c9fca23373dc3999575555b5ec
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2020-03-23 14:23:00 -0500

    removed insta code from my branch, trying to avoid merge conflicts down the road!

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import React, { useEffect } from "react";
-// import GetInstagramPosts from "./Services/Instagram";
 import Youtube from "./Components/Social/Youtube";
-// import Details from "./Details";
 
 const App = () => {
-  useEffect(() => {
-    // GetInstagramPosts("gernaderjake");
-  });
-
   return (
     <div className="App">
       <p>app div</p>
