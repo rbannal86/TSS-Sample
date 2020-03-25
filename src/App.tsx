3287f0d985987efe32840bed6500fffde4388140
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2020-03-25 10:25:51 -0500

    basic copyright footer in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Youtube } from "./Components/Social/Youtube";
 import { Twitch } from "./Components/Social/Twitch";
+import { Footer } from "./Components/Footer/Footer";
 
 import "./Components/Social/Social.css";
+import "./Components/Footer/Footer.css";
 
 const App = () => {
   return (
     <div className="App">
       <Youtube />
       <Twitch />
+      <Footer />
     </div>
   );
 };
